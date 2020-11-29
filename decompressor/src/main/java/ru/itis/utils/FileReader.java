6e5c75174043bd667cff485da864640e5598a778
Sumbel <sumbel.enikeeva@gmail.com>
2020-11-29 15:58:11 +0300

    decompressor utils

--- decompressor/src/main/java/ru/itis/utils/FileReader.java
+++ decompressor/src/main/java/ru/itis/utils/FileReader.java
@@ -2,77 +2,11 @@ package ru.itis.utils;
 
 import ru.itis.structures.LZ78Node;
 import ru.itis.structures.LZ78Output;
-import ru.itis.structures.Word;
 
 import java.io.*;
 import java.util.*;
 
 public class FileReader {
-    public List<Integer> getIntArrayOfFile(String filepath) throws IOException {
-        BufferedReader reader = new BufferedReader(new java.io.FileReader(filepath));
-        int symbol;
-        List<Integer> intList = new ArrayList<>();
-        while ((symbol = reader.read()) != -1) {
-            intList.add(symbol);
-        }
-//        intList.add(-1);
-        return intList;
-    }
-
-    public List<List<Integer>> getIntsOfWords(String filepath) throws IOException {
-        BufferedReader reader = new BufferedReader(new java.io.FileReader(filepath));
-        int symbol;
-        List<List<Integer>> words = new ArrayList<>();
-        List<Integer> word = new ArrayList<>();
-        while ((symbol = reader.read()) != -1) {
-            if ((symbol >= 1 && symbol <= 64) || (symbol >= 91 && symbol <= 96)) {
-                if (word.size() > 0) {
-                    words.add(word);
-                }
-                words.add(Collections.singletonList(symbol));
-                word = new ArrayList<>();
-            } else {
-                word.add(symbol);
-            }
-        }
-        return words;
-    }
-
-    public List<List<Integer>> getIntPacks(String filepath, int n) throws IOException {
-        BufferedReader reader = new BufferedReader(new java.io.FileReader(filepath));
-        int symbol;
-        List<List<Integer>> words = new ArrayList<>();
-        List<Integer> word;
-        boolean flag = true;
-        while (flag) {
-            word = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                symbol = reader.read();
-                if (symbol == -1) {
-                    words.add(word);
-                    flag = false;
-                }
-                word.add(symbol);
-            }
-            words.add(word);
-        }
-        return words;
-    }
-
-    public List<Word> getWords(String filepath) throws IOException {
-        BufferedReader reader = new BufferedReader(new java.io.FileReader(filepath));
-        int symbol;
-        List<Word> words = new ArrayList<>();
-        List<Integer> word = new ArrayList<>();
-        while ((symbol = reader.read()) != -1) {
-            if (symbol >= 32 && symbol <= 64) {
-                words.add(Word.builder().wordInts(word).build());
-                words.add(Word.builder().wordInts(Collections.singletonList(symbol)).build());
-                word = new ArrayList<>();
-            }
-        }
-        return words;
-    }
 
     public LZ78Output read7Z78(String filepath) throws IOException {
         int fileSize;
