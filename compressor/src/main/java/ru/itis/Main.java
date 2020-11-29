c2a14d66ab7c867bd7029488ba2b10201c77d87f
Sumbel <sumbel.enikeeva@gmail.com>
2020-11-29 16:00:50 +0300

    compressor main

--- compressor/src/main/java/ru/itis/Main.java
+++ compressor/src/main/java/ru/itis/Main.java
@@ -1,6 +1,8 @@
 package ru.itis;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import ru.itis.structures.LZ78Output;
 import ru.itis.utils.BitsSaver;
 import ru.itis.utils.FileReader;
@@ -9,18 +11,22 @@ import ru.itis.utils.LZ78;
 import java.io.IOException;
 import java.util.List;
 
-public class LZ78Test {
+public class Main {
+    @Parameter(names = "-i")
+    private String input;
+
+    @Parameter(names = "-o")
+    private String output;
     public static void main(String[] args) throws IOException {
         Main mainArg = new Main();
         JCommander.newBuilder()
                 .addObject(mainArg)
                 .build()
                 .parse(args);
-        String filepath = "voina_i_mir.txt";
 
         FileReader fileReader = new FileReader();
         BitsSaver bitsSaver = new BitsSaver();
-        List<Integer> fileInts = fileReader.getIntArrayOfFile(filepath);
+        List<Integer> fileInts = fileReader.getIntArrayOfFile(mainArg.input);
 
         LZ78 lz78 = new LZ78();
 
@@ -30,7 +36,7 @@ public class LZ78Test {
 
         List<Integer> bitsArray = lz78.toBitsArray(result);
 
-        bitsSaver.lz78Save(bitsArray, "lz78.dat");
+        bitsSaver.lz78Save(bitsArray, mainArg.output);
 
     }
 
